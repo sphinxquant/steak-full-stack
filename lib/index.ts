1838cb10b0bc67165d15eaef2545af2b8603d7b7
Kitty Adventures <kittyadventures55@gmail.com>
2020-12-26 22:04:36 -0500

    docker ports forced forward

--- lib/index.ts
+++ lib/index.ts
@@ -6,7 +6,10 @@ const CreateServer = () => {
     const app = express();
     const PORT = 8000;
     
-    app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+    app.get('/', (req, res) => {
+        console.log('message recieved');
+        res.send('Express + TypeScript Server');
+    });
     
     app.listen(PORT, () => {
       console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
